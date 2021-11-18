2359dc3af1468b476289d93d7054ac0021fa3ea2
Cristopher Fontes <cris.fontstudy@gmail.com>
2021-11-18 15:26:41 -0300

    Creo el archivo de Package.json y el index.ts

---
+++ index.js
@@ -0,0 +1,13 @@
+var nombre;
+nombre = "Cristopher Fontes";
+var edad;
+edad = 26;
+var personaje = {
+    nombre: nombre,
+    edad: edad
+};
+var spiderman = {
+    nombre: "Peter parket",
+    poderes: ["Trepar", "Trepar"]
+};
+console.log(spiderman);
---
+++ index.ts
@@ -0,0 +1,18 @@
+
+
+interface auto {
+    nombre: string,
+    modelo: string,
+    color: string,
+    velocidad_maxima: number,
+    numeroDePuertas: number
+    
+
+}
+
+
+var  : auto = {
+    nombre: 
+    poderes: 
+}
+
