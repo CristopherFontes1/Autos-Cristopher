b8e2868103164f09224d76ae4867cd8a708a7e58
Cristopher Fontes <cris.fontstudy@gmail.com>
2021-11-18 19:47:07 -0300

    Modifique el index.ts lo traspile a .js, dentro de index.ts se encuentra una interfaz que permite crear Autos, luego genere algunos console.log para mostrar por consola algunos datos

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
-var nombre;
-nombre = "Cristopher Fontes";
-var edad;
-edad = 26;
-var personaje = {
-    nombre: nombre,
-    edad: edad
+var Enzo = {
+    fabricante: "Ferrari",
+    modelo: "A31",
+    color: "Rojo",
+    velocidad_maxima: 351,
+    numeroDePuertas: 2,
+    precio: 2640000
 };
-var spiderman = {
-    nombre: "Peter parket",
-    poderes: ["Trepar", "Trepar"]
+var LaFerrari = {
+    fabricante: "Ferrari",
+    modelo: "A31",
+    color: "Amarillo",
+    velocidad_maxima: 349,
+    numeroDePuertas: 2,
+    precio: 1416362
 };
-console.log(spiderman);
+var Lusso = {
+    fabricante: "Ferrari",
+    modelo: "GTC4",
+    color: "Gris",
+    velocidad_maxima: 320,
+    numeroDePuertas: 2,
+    precio: 300000
+};
+var Portofino = {
+    fabricante: "Ferrari",
+    modelo: "C35",
+    color: "Rojo",
+    velocidad_maxima: 320,
+    numeroDePuertas: 2,
+    precio: 218750
+};
+console.log("El precio del Ferrari Ezon es: " + Enzo.precio);
+console.log("El auto con mayor velocidad es Enzo " + Math.max(Enzo.velocidad_maxima, LaFerrari.velocidad_maxima, Lusso.velocidad_maxima, Portofino.velocidad_maxima) + "km/h");
--- index.ts
+++ index.ts
@@ -1,18 +1,54 @@
 
 
 interface auto {
-    nombre: string,
+    fabricante: string,
     modelo: string,
     color: string,
     velocidad_maxima: number,
     numeroDePuertas: number
+    precio: number
     
 
 }
 
 
-var  : auto = {
-    nombre: 
-    poderes: 
+var Enzo : auto = {
+    fabricante: "Ferrari",
+    modelo: "A31",
+    color: "Rojo",
+    velocidad_maxima: 351,
+    numeroDePuertas: 2,
+    precio: 2640000
 }
 
+var LaFerrari : auto = {
+    fabricante: "Ferrari",
+    modelo: "A31",
+    color: "Amarillo",
+    velocidad_maxima: 349,
+    numeroDePuertas: 2,
+    precio:  1416362
+}
+
+var Lusso : auto = {
+    fabricante: "Ferrari",
+    modelo: "GTC4",
+    color: "Gris",
+    velocidad_maxima: 320,
+    numeroDePuertas: 2,
+    precio: 300000
+}
+
+var Portofino : auto = {
+    fabricante: "Ferrari",
+    modelo: "C35",
+    color: "Rojo",
+    velocidad_maxima: 320,
+    numeroDePuertas: 2,
+    precio: 218750
+}
+
+
+console.log("El precio del Ferrari Ezon es: " + Enzo.precio);
+
+console.log("El auto con mayor velocidad es Enzo "+ Math.max(Enzo.velocidad_maxima,LaFerrari.velocidad_maxima,Lusso.velocidad_maxima,Portofino.velocidad_maxima) + "km/h")
